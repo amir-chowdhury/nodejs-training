2730716f15914d57052aa86e94020c68ed5044d3
Amir_Chowdhury <Amir_Chowdhury@epam.com>
2021-10-04 22:52:03 +0100

    refactor: pass port as an argument to startServer

--- src/start.ts
+++ src/start.ts
@@ -3,17 +3,22 @@ import { env } from 'process'
 import logger from 'loglevel'
 import { getRoutes } from './api/v1/routes'
 import { errorMiddleware } from './api/v1/middlewares/error'
+import { Server } from 'http'
 
-function startServer (): void {
+async function startServer (
+  { port = env.PORT ?? 3000 } = {}
+): Promise<Server> {
   const app = express()
-  const PORT = env.PORT ?? 3000
-  app.set('port', PORT)
+  app.set('port', port)
   app.use(express.json())
   app.use('/api/v1', getRoutes())
   app.use(errorMiddleware)
 
-  app.listen(PORT, () => {
-    logger.info(`⚡️[server]: Server is running at http://localhost:${PORT}`)
+  return await new Promise((resolve) => {
+    const server = app.listen(port, () => {
+      logger.info(`⚡️[server]: Server is running at http://localhost:${port}`)
+      resolve(server)
+    })
   })
 }
 
